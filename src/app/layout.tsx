05aa99c2836d488dcb62dfbc31330c4dc7c8a112
Adrian Raduti <79102923+araduti@users.noreply.github.com>
2025-08-26 14:02:39 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "~/styles/globals.css";
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 
-import { TRPCReactProvider } from "~/trpc/react";
+import { TRPCReactProvider } from "@/trpc/react";
 
 export const metadata: Metadata = {
   title: "rdr.nu - Lightning Fast URL Shortener",
